9e7899815cb367d37a69558e9f1518bb8f410a95
Abdulloh <abdulloh50007@gmail.com>
2025-06-20 18:48:36 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <EmeraldCoin shape

--- src/components/coinshapes/DiamondCoin.tsx
+++ src/components/coinshapes/DiamondCoin.tsx
@@ -17,10 +17,10 @@ const DiamondCoin: React.FC<CoinShapeProps> = ({ shapeFillClass, iconClass, clas
         className={cn("absolute inset-0 w-full h-full", shapeFillClass)} // Apply fill class to SVG
         preserveAspectRatio="xMidYMid meet"
       >
-        {/* Simplified diamond shape */}
+        {/* Diamond shape - points already utilize viewBox well */}
         <polygon points="50,2 98,35 90,95 10,95 2,35" />
       </svg>
-      <Coins className={cn("w-3/5 h-3/5 relative z-10", iconClass)} /> {/* Icon on top */}
+      <Coins className={cn("w-3/4 h-3/4 relative z-10", iconClass)} /> {/* Icon on top, size increased */}
     </div>
   );
 };
--- src/components/coinshapes/EmeraldCoin.tsx
+++ src/components/coinshapes/EmeraldCoin.tsx
@@ -17,10 +17,10 @@ const EmeraldCoin: React.FC<CoinShapeProps> = ({ shapeFillClass, iconClass, clas
         className={cn("absolute inset-0 w-full h-full", shapeFillClass)} // Apply fill class to SVG
         preserveAspectRatio="xMidYMid meet"
       >
-        {/* Emerald-like octagon shape */}
-        <polygon points="25,5 75,5 95,25 95,75 75,95 25,95 5,75 5,25" />
+        {/* Adjusted points for a larger visual shape */}
+        <polygon points="20,2 80,2 98,22 98,78 80,98 20,98 2,78 2,22" />
       </svg>
-      <Coins className={cn("w-3/5 h-3/5 relative z-10", iconClass)} /> {/* Icon on top */}
+      <Coins className={cn("w-3/4 h-3/4 relative z-10", iconClass)} /> {/* Icon on top, size increased */}
     </div>
   );
 };
